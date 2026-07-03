bc857555630b0bbcc4df456d4a9cf95459f474f1
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-07-03 23:47:26 +0530

    Time: 1 ms (100%), Space: 43.6 MB (88.36%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,37 +1,63 @@
 class Solution {
-
     public int myAtoi(String s) {
 
         int i = 0;
         int n = s.length();
-        int sign = 1;
-        int ans = 0;
 
-        //skip spaces
+        // Skip leading spaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
 
-        //check sign
+        // Store sign (default positive)
+        int sign = 1;
+
+        // Check for '+' or '-'
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') sign = -1;
-            i++;
-        }
 
-        //convert digits
-        while (i < n && Character.isDigit(s.charAt(i))) {
-
-            int digit = s.charAt(i) - '0';
-
-            // check overflow BEFORE adding
-            if (ans > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if (s.charAt(i) == '-') {
+                sign = -1;
             }
 
-            ans = ans * 10 + digit;
             i++;
         }
 
-        return ans * sign;
+        long num = 0;
+
+        // Read digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            // Convert character to digit
+            int digit = s.charAt(i) - '0';
+
+            // Build number
+            num = num * 10 + digit;
+
+            // Check overflow
+            if (sign * num > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if (sign * num < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        // Return final answer
+        return (int)(sign * num);
     }
 }
+/*
+Decimal to Digit Mapping (ASCII Standard)
+48 → '0'
+49 → '1'
+50 → '2'
+51 → '3'
+52 → '4'
+53 → '5'
+54 → '6'
+55 → '7'
+56 → '8'
+57 → '9'
+
+*/
