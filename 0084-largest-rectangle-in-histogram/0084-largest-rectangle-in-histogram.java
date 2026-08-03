0c20fab1b754d82bb62ba57b3ac08649fd220417
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-08-03 16:09:52 +0530

    Time: 120 ms (6.09%), Space: 76.4 MB (84.02%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,13 +1,23 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int maxArea = 0;
+
+        //PSE[i] batata hai ki left me kaha tak extend kar sakte ho.
+        //NSE[i] batata hai ki right me kaha tak extend kar sakte ho.
         int[] nse = nse(heights);
         int[] pse = pse(heights);
-
+        
+        //Dono boundaries ke beech ki width nikal lo.
         for(int i=0; i<heights.length; i++){
+
+            // Rectangle starts from (PSE + 1) and ends at (NSE - 1)
+            // Width = (NSE - 1) - (PSE + 1) + 1 = NSE - PSE - 1
             int width = nse[i]-pse[i]-1;
-            int curr = heights[i]*width;
-            maxArea = Math.max(maxArea, curr);
+            
+            //height × width se us bar ka maximum rectangle area mil jayega.
+            int area = heights[i]*width; 
+            
+            maxArea = Math.max(maxArea, area);
         }
         return maxArea;
     }
@@ -21,10 +31,14 @@ class Solution {
             while(!st.isEmpty() && height[st.peek()] >= height[i]){
                 st.pop();
             }
+
             if(st.isEmpty()){
                 pse[i] = -1;
             }
-            else pse[i] = st.peek();
+
+            else{ 
+                pse[i] = st.peek();
+            }
 
             st.push(i);
         }
@@ -43,9 +57,11 @@ class Solution {
 
             if (st.isEmpty())
                 nse[i] = height.length;
-            else
-                nse[i] = st.peek();
 
+            else{
+                nse[i] = st.peek();
+            } 
+            
             st.push(i);
         }
 
